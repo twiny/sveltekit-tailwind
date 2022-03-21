41b97d46b87916f89eae73a12a4184c66cab4cbc
Iss M <59224605+twiny@users.noreply.github.com>
2022-03-21 18:36:31 +0100

    updated dependency

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter(),
-		appDir: '_app',
-		files: {
-			assets: 'static',
-		}
-	}
+    kit: {
+        // hydrate the <div id="svelte"> element in src/app.html
+        adapter: adapter(),
+        appDir: '_app',
+        files: {
+            assets: 'static',
+        }
+    }
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,50 +2,50 @@ const colors = require('tailwindcss/colors')
 
 // tailwind.config.cjs
 module.exports = {
-  mode: 'jit',
-  // you dont need `purge: enabled: production` because you are using jit
-  purge: [
-    "./src/**/*.svelte",
-    "./src/**/*.html",
-    "./public/index.html"
-  ],
-  darkMode: 'class',
-  theme: {
-    screens: {
-      'sm': '640px',      // => @media (min-width: 640px) { ... }
-      'md': '768px',      // => @media (min-width: 768px) { ... }
-      'lg': '1024px',     // => @media (min-width: 1024px) { ... }
-      'xl': '1280px',     // => @media (min-width: 1280px) { ... }
-      '2xl': '1536px',    // => @media (min-width: 1536px) { ... }
+    mode: 'jit',
+    // you dont need `purge: enabled: production` because you are using jit
+    content: [
+        "./src/**/*.svelte",
+        "./src/**/*.html",
+        "./public/index.html"
+    ],
+    darkMode: 'class',
+    theme: {
+        screens: {
+            'sm': '640px', // => @media (min-width: 640px) { ... }
+            'md': '768px', // => @media (min-width: 768px) { ... }
+            'lg': '1024px', // => @media (min-width: 1024px) { ... }
+            'xl': '1280px', // => @media (min-width: 1280px) { ... }
+            '2xl': '1536px', // => @media (min-width: 1536px) { ... }
+        },
+        colors: {
+            black: colors.black,
+            white: colors.white,
+            rose: colors.rose,
+            pink: colors.pink,
+            fuchsia: colors.fuchsia,
+            purple: colors.purple,
+            violet: colors.violet,
+            indigo: colors.indigo,
+            blue: colors.blue, // colors.lightBlue,
+            cyan: colors.cyan,
+            teal: colors.teal,
+            emerald: colors.emerald,
+            green: colors.green,
+            lime: colors.lime,
+            yellow: colors.yellow,
+            amber: colors.amber,
+            orange: colors.orange,
+            red: colors.red,
+            gray: colors.gray, // warmGray, trueGray, coolGray, blueGray
+        },
+        extend: {},
     },
-    colors: {
-      black: colors.black,
-      white: colors.white,
-      rose: colors.rose,
-      pink: colors.pink,
-      fuchsia: colors.fuchsia,
-      purple: colors.purple,
-      violet: colors.violet,
-      indigo: colors.indigo,
-      blue: colors.blue, // colors.lightBlue,
-      cyan: colors.cyan,
-      teal: colors.teal,
-      emerald: colors.emerald,
-      green: colors.green,
-      lime: colors.lime,
-      yellow: colors.yellow,
-      amber: colors.amber,
-      orange: colors.orange,
-      red: colors.red,
-      gray: colors.gray, // warmGray, trueGray, coolGray, blueGray
-    },
-    extend: {},
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-  corePlugins: {
-    outline: false,
-  }
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+    corePlugins: {
+        outline: false,
+    }
 }
